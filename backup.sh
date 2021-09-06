6c1cb95b1acdc306d61836203be2ff4adbb4d471
henrikbeck95 <henrikbeck95@gmail.com>
2021-09-06 15:33:19 -0300

    Add tools_replace_current method for backup.sh file

--- backup.sh
+++ backup.sh
@@ -13,7 +13,7 @@ MESSAGE_HELP="
 -e\t--edit\t\t\t\tEdit this script file
 -bc\t--backup-create\t\t\tCreate a Dotfiles backup
 -bl\t--backup-list\t\t\tList the Dotfiles backups
--br\t--backup-restore\t\tRestore a Dotfile backup (NOT IMPLEMENTED YET)
+-br\t--backup-restore\t\tRestore a Dotfile backup
 -r\t--replace\t\t\tReplace the current dotfiles by the compiled ones
 "
 
@@ -29,14 +29,20 @@ tools_backup_create(){
 }
 
 tools_backup_list(){
-	for BACKUP_VERSION in $(ls $HOME/.dotfiles/backup/); do
+	for BACKUP_VERSION in $(ls $DOTFILES_BACKUP/); do
 		local DATE_TIMESTAMP_VERSION="$BACKUP_VERSION"
 		local DATE_TIMESTAMP_REVERSE=$(date -d "@$DATE_TIMESTAMP_VERSION")
 		echo -e "$DATE_TIMESTAMP_VERSION:\t$DATE_TIMESTAMP_REVERSE"
 	done;
 }
 
-#tools_backup_restore(){}
+tools_backup_restore(){
+	#Get last backup version
+	local BACKUP_VERSION_LATEST=$(ls $DOTFILES_BACKUP/ | tail -1)
+
+	#Replace the current version by the backup version
+	cp $DOTFILES_BACKUP/$BACKUP_VERSION_LATEST/* $DOTFILES_CURRENT/
+}
 
 tools_replace_current(){
 	tools_backup_create
@@ -50,7 +56,7 @@ case $AUX1 in
 	"-e" | "--edit") $EDITOR $0 ;;
 	"-bc" | "--backup-create") tools_backup_create ;;
 	"-bl" | "--backup-list") tools_backup_list ;;
-	#"-br" | "--backup-restore") tools_backup_restore ;;
+	"-br" | "--backup-restore") tools_backup_restore ;;
 	"-r" | "--replace") tools_replace_current ;;
 	*) echo -e "$MESSAGE_ERROR" ;;
 esac
